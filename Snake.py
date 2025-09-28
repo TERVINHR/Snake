91addbc573e5ab3a7b8738f441640da049a71378
TERVINHR <tvnhrishikesh@gmail.com>
2025-09-28 21:05:47 +0530

    Snake.py
    Can someone find out why the snake shed skin sometimes?

--- Snake.py
+++ Snake.py
@@ -3,75 +3,107 @@ import random
 import keyboard
 import time
 STATE = 1
-height = 30
-width = 75
-empty = ' '
-sbody = '☐'
-food = 'O'
+speed = 1
+HEIGHT = 30
+WIDTH = 75
+EMPTY = ' '
+SBODY = '☐'
+FOOD = '\b🥚'
+VITAMIN_D = '\b🔴'
+FOODS=[FOOD,VITAMIN_D]
+t_decay = 0
+nfspawn = 0 #no: of food spawn
 sx,sy = 38,15
 tx,ty = 37,15
-turn_points = [[1,0]]
+turn_points  = [[1,0]]
 dir=[1,0]
 grow = False
 score = 0
-screen=[[empty for _ in range(height)] for _ in range(width)]
-screen[sx][sy]=sbody
-screen[tx][ty]=sbody
-for i in range(width):
-        for j in range(height):
-            screen[i][j]=empty
-fx = random.randint(0,width-1)
-fy = random.randint(0,height-1)
-while(screen[fx][fy]!=empty):
-    fx = random.randint(0,width-1)
-    fy = random.randint(0,height-1)
-screen[fx][fy]=food
-def update(screen,sx,sy,tx,ty,score,STATE,turn_points,v):
+screen=[[EMPTY for _ in range(HEIGHT)] for _ in range(WIDTH)]
+screen[sx][sy]=SBODY
+screen[tx][ty]=SBODY
+for i in range(WIDTH):
+        for j in range(HEIGHT):
+            screen[i][j]=EMPTY
+food_note='EAT ME!'
+vit_note='EAT YOUR VITAMINS ON TIME!'
+
+def spawn_food(fn = 0):
+    fx = random.randint(0,WIDTH-1)
+    fy = random.randint(0,HEIGHT-1)
+    while(screen[fx][fy]!=EMPTY):
+        fx = random.randint(0,WIDTH-1)
+        fy = random.randint(0,HEIGHT-1)
+    screen[fx][fy] = FOOD
+    if(fn):
+        for l in range(len(food_note)):
+            screen[(fx+1+l)%WIDTH][fy]=food_note[l]
+def spawn_vit(fn = 0):
+    fx = random.randint(0,WIDTH-1)
+    fy = random.randint(0,HEIGHT-1)
+    while(screen[fx][fy]!=EMPTY):
+        fx = random.randint(0,WIDTH-1)
+        fy = random.randint(0,HEIGHT-1)
+    screen[fx][fy] = VITAMIN_D
+    if(fn):
+        for l in range(len(food_note)):
+            screen[(fx+1+l)%WIDTH][fy]=food_note[l]
+def update(screen,sx,sy,tx,ty,score,t_decay,nfspawn,STATE,turn_points,v):
     grow = False
     sx+=v[0]
     sy+=v[1]
-    sx%=width
-    sy%=height
-    if screen[sx][sy]==empty:
-        screen[sx][sy]=sbody
-    elif screen[sx][sy]==food:
-        screen[sx][sy]=sbody
-        grow = True
-        score+=1
-        fx = random.randint(0,width-1)
-        fy = random.randint(0,height-1)
-        while(screen[fx][fy]!=empty):
-            fx = random.randint(0,width-1)
-            fy = random.randint(0,height-1)
-        screen[fx][fy] = food
-    elif screen[sx][sy] == sbody:
+    sx%=WIDTH
+    sy%=HEIGHT
+    if t_decay>0: t_decay-=1
+    if screen[sx][sy]==EMPTY:
+        screen[sx][sy]=SBODY
+    elif screen[sx][sy] in FOODS or screen[sx][sy] in food_note:
+        if(screen[sx][sy] in FOODS):
+            nfspawn+=1
+            score+=1 + (screen[sx][sy]==VITAMIN_D)*((t_decay-t_decay%5)/5)
+            if((screen[sx][sy]==VITAMIN_D)):t_decay = 0
+            grow = True
+            if nfspawn%5!=0:
+                spawn_food()
+            else:
+                t_decay = 50
+                spawn_vit()
+        screen[sx][sy]=SBODY
+    elif screen[sx][sy] == SBODY:
        STATE = 0
-    if grow == False:
-        if(screen[(tx+turn_points[0][0])%width][(ty+turn_points[0][1])%height] != '☐' and len(turn_points)>1):
+    if(screen[(tx+turn_points[0][0])%WIDTH][(ty+turn_points[0][1])%HEIGHT] != SBODY and len(turn_points)>1):
                 turn_points.pop(0)
+    if grow == False:
         tx+=turn_points[0][0]
         ty+=turn_points[0][1]
-        tx%=width
-        ty%=height
-        if(screen[tx][ty] == sbody):
-            screen[tx][ty]=empty
+        tx%=WIDTH
+        ty%=HEIGHT
+        if(screen[tx][ty] == SBODY):
+            screen[tx][ty]=EMPTY
     else:
          grow = False
-    return screen,sx,sy,tx,ty,score,STATE,turn_points
+    return screen,sx,sy,tx,ty,score,t_decay,nfspawn,STATE,turn_points
 def print_Screen(S):
     print('score : ' , score)
+    if t_decay>0:
+        for _ in range(t_decay): print('🟥',end='')
+    else:
+        print('Controls : ↑ ↓ → ← space')
     for i in range(len(S[0])):
         for j in range(len(S)):
             print(S[j][i],end=' ')
         print()
-a = input("Set to full screen")
+input("Press Enter to start (Set to full screen first)...")
+spawn_food(1)
 while(STATE):
-    #a = input("Next")
-    os.system('cls')
-    
+    os.system('cls' if os.name == 'nt' else 'clear')
     print_Screen(screen)
+
     dir_ = dir
-    time.sleep(0.2)
+    if keyboard.is_pressed('space'):
+        time.sleep(0.05/speed)
+    else:
+        time.sleep(0.2/speed)
     if keyboard.is_pressed('left') and dir[0]!=1:
         dir=[-1,0]
     elif keyboard.is_pressed('right') and dir[0]!=-1:
@@ -84,9 +116,11 @@ while(STATE):
     #print(screen[tx][ty],turn_points)
     if dir!=dir_:
          turn_points.append(dir)
-    screen,sx,sy,tx,ty,score,STATE,turn_points=update(screen,sx,sy,tx,ty,score,STATE,turn_points,turn_points[-1])
-os.system('cls')
-print("GAME OVER")
+    screen,sx,sy,tx,ty,score,t_decay,nfspawn,STATE,turn_points=update(screen,sx,sy,tx,ty,score,t_decay,nfspawn,STATE,turn_points,turn_points[-1])
+os.system('cls' if os.name == 'nt' else 'clear')
+print("GAME OVER !")
 print_Screen(screen)
+input("Press Enter to exit...")
+
     
     
